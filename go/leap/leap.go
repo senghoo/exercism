57922a14ee5d6065dc121edc26d6a64321697bd1
Senghoo Kim <shkdmb@gmail.com>
2016-01-11 21:25:13 +0800

    init with first go exercise

---
+++ go/leap/leap.go
@@ -0,0 +1,29 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// This next comment is a "build constraint."  It's here for, well, kind of
+// complicated testing purposes you don't need to worry about now.
+// Actually, you can delete it.
+
+// +build !example
+
+// The package name is expected by the test program.
+// It has to stay just the way it is.
+package leap
+
+// TestVersion identifies the version of the test program that you are
+// writing your code to.  If the test program changes in the future --
+// after you have posted this code to the Exercism site -- reviewers
+// will see that your code can't necessarily be expected to pass the
+// current test suite because it was written to an earlier test version.
+const TestVersion = 1
+
+// It's good style to write a comment here documenting IsLeapYear.
+// (But delete all these instructional comments!)
+func IsLeapYear(int) bool {
+	// Write some code here to pass the test suite.
+
+	// When you have a working solution, REMOVE ALL THE STOCK COMMENTS.
+	// They're here to to help you get started but they only clutter a finished solution.
+	// If you leave them in, reviewers will protest!
+}
