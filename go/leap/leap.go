2d8447cf841dc9e2e2246ddb1df891589825f828
Senghoo Kim <shkdmb@gmail.com>
2016-01-12 21:40:10 +0800

    Refactoring leap

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,14 +3,9 @@ package leap
 const TestVersion = 1
 
 func IsLeapYear(year int) bool {
-	switch {
-	case year%400 == 0:
-		return true
-	case year%100 == 0:
-		return false
-	case year%4 == 0:
-		return true
-	default:
-		return false
+	divisbleBy := func(a, b int) bool {
+		return (a % b) == 0
 	}
+
+	return divisbleBy(year, 4) && (!divisbleBy(year, 100) || divisbleBy(year, 400))
 }
