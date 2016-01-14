7109fed91c7e849525701bbd5a624f555538d085
Senghoo Kim <shkdmb@gmail.com>
2016-01-14 23:07:21 +0800

    finish secret-handshake

---
+++ go/secret-handshake/secret_handshake.go
@@ -0,0 +1,39 @@
+package secret
+
+var secretCode = []struct {
+	code int
+	h    string
+}{
+	{1, "wink"},
+	{2, "double blink"},
+	{4, "close your eyes"},
+	{8, "jump"},
+}
+
+var (
+	secretLen int = len(secretCode)
+	reverse   int = 1 << uint(secretLen)
+	all_code  int = reverse - 1
+)
+
+func Handshake(val int) []string {
+	res := make([]string, 0, 4)
+
+	if val <= 0 || all_code&val == 0 {
+		return nil
+	}
+
+	for i := 0; i < secretLen; i++ {
+		// index in secretCode
+		index := i
+		// if reverse
+		if reverse&val != 0 {
+			index = secretLen - i - 1
+		}
+
+		if secretCode[index].code&val != 0 {
+			res = append(res, secretCode[index].h)
+		}
+	}
+	return res
+}
