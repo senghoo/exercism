4583f59040c71d85c3fc6fb2c01d85d0e8fda18d
Senghoo Kim <shkdmb@gmail.com>
2016-01-24 20:30:09 +0800

    pass the word-is-not-own-anagram test

--- elisp/anagram/anagram.el
+++ elisp/anagram/anagram.el
@@ -8,11 +8,12 @@
 
 
 (defun string-to-ordered-list (string)
-  (sort (string-to-list string ) '<))
+  (sort (string-to-list string) '<))
 
 (defun anagrams-p (word1 word2)
-  (equal (string-to-ordered-list word1)
-         (string-to-ordered-list word2)))
+  (and (not (equal word1 word2))
+       (equal (string-to-ordered-list word1)
+              (string-to-ordered-list word2))))
 
 (defun anagrams-for (word anagrams)
   (cl-remove-if-not (lambda (x) (anagrams-p (downcase word) (downcase x)))
