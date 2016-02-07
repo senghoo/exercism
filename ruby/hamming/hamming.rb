fac6542bfda4d16a882db05f48ed906bed741b96
Senghoo Kim <shkdmb@gmail.com>
2016-02-07 23:47:46 +0800

    finish hamming ruby version

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -6,7 +6,9 @@ class String
 end
 
 class Hamming
+  VERSION = 1
   def self.compute(a, b)
-    a.to_a.zip(b.to_a).collect { |j, k| j == k ? 0 : 1}.inject(:+)
+    fail ArgumentError if a.length != b.length
+    a.to_a.zip(b.to_a).collect { |j, k| j == k ? 0 : 1}.inject(:+) || 0
   end
 end
